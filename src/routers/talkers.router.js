3e4c942adba97e414e3103b52c8a0a5331d4dd23
Valéria <valeriamenezes022@gmail.com>
2022-12-14 17:30:39 -0300

    feat: Crie o endpoint GET /talker/:id

--- src/routers/talkers.router.js
+++ src/routers/talkers.router.js
@@ -9,4 +9,17 @@ talkerRouter.get('/talker', async (req, res) => {
   return res.status(200).json(talkers);
 });
 
+talkerRouter.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+
+    const talkers = await getAllTalkers();
+    console.log(talkers);
+    
+    const filterById = talkers.find((talker) => talker.id === Number(id));
+    if (!filterById) {
+      return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    }
+    return res.status(200).json(filterById);
+});
+
 module.exports = talkerRouter;
